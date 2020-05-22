2bcbaea9d69b55c60f56c79fb15ca48a44e9b3a9
artufaro <artolfi@gmail.com>
2020-05-22 18:05:53 +0200

    spin-waves with TOF

---
+++ programs/subroutines/magnTOF_imaging.py
@@ -0,0 +1,27 @@
+prg_comment = ""
+prg_version = "0.7"
+def program(prg, cmd):
+    prg.add(-3250000, "TTL MirrorBottom Probe")
+    prg.add(-3012000, "Shutter Probe Vert ON")
+    prg.add(-10000, "TTL Picture Hamamatsu  ON", 'PTAI_p1')
+    prg.add(-8000, "TTL Picture Hamamatsu OFF")
+    prg.add(-4000, "TTL ProbeVert OFF")
+    prg.add(-3990, "TTL ProbeHor OFF")
+    prg.add(-3500, "AOM Probe Amp ch1 (+)", 1000)
+    prg.add(-3000, "AOM Probe Amp ch2 (-)", 1000)
+    prg.add(-400, "Oscilloscope Trigger ON", enable=False)
+    prg.add(-100, "transfer_m1to0", enable=False)
+    prg.add(-100, "transfer_p1to0")
+    prg.add(-100, "transfer_0to0", enable=False)
+    prg.add(0, "TTL ProbeVert ON")
+    prg.add(50, "TTL ProbeVert OFF")
+    prg.add(8500, "TTL Picture Hamamatsu  ON", 'PTAI_m1')
+    prg.add(9550, "transfer_p1to0", enable=False)
+    prg.add(10300, "TTL Picture Hamamatsu OFF")
+    prg.add(10400, "transfer_m1to0")
+    prg.add(10500, "TTL ProbeVert ON")
+    prg.add(10550, "TTL ProbeVert OFF")
+    prg.add(11500, "AOM Probe Amp ch1 (+)", 0)
+    prg.add(12000, "AOM Probe Amp ch2 (-)", 0)
+    prg.add(12500, "Oscilloscope Trigger OFF", enable=False)
+    return prg
